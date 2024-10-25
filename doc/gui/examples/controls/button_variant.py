1801732f793ac403935be52d73dedca86c8a26fc
JeevaRamanathan M <jeevaramanathan.m@infosys.com>
2024-10-25 19:00:31 +0000

    removed empty line
    Signed-off-by: JeevaRamanathan M <jeevaramanathan.m@infosys.com>

--- doc/gui/examples/controls/button_variant.py
+++ doc/gui/examples/controls/button_variant.py
@@ -15,7 +15,6 @@
 # -----------------------------------------------------------------------------------------
 from taipy.gui import Gui
 
-
 page = """
  "<|Button Label|button|variant=outlined|>"
 """
