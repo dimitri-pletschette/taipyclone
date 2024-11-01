b3eea77e2134b622a1a53d8fb3170bd32108ee31
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2024-11-01 16:38:37 +0100

    file_selector example make State inherit from SimpleNamespace so that the linters don't mark state.var as an error

---
+++ doc/gui/examples/controls/file_selector_simple.py
@@ -0,0 +1,33 @@
+# Copyright 2021-2024 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+# -----------------------------------------------------------------------------------------
+# To execute this script, make sure that the taipy-gui package is installed in your
+# Python environment and run:
+#     python <script>
+# -----------------------------------------------------------------------------------------
+import taipy.gui.builder as tgb
+from taipy.gui import Gui, State
+
+filename = "<unknown>"
+
+
+def print_filename(state: State):
+    print("filename is ", state.filename)  # noqa: T201
+
+def on_change(state: State, var: str, val: str):
+    print(f"on_change(state: State, var: {var}, val: {val})")  # noqa: T201
+
+with tgb.Page() as page:
+    tgb.file_selector("{filename}", on_action=print_filename)
+    tgb.text("selected File: {filename}")
+
+if __name__ == "__main__":
+    Gui(page).run(title="File Selector - Simple")
