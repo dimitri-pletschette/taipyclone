81d30e937edb41aa1aea7900394b4d9e33297a03
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2024-11-20 14:29:08 +0100

    ruff

--- doc/gui/examples/controls/status_mixed_icons.py
+++ doc/gui/examples/controls/status_mixed_icons.py
@@ -14,7 +14,7 @@ status = [
 # error: inline svg icon (red disc)
 page = """
 <|{status}|status|icons=https://www.svgrepo.com/show/530594/pants.svg;false;true;<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24'><circle cx='12' cy='12' r='10' fill='red'/></svg>|>
-"""
+"""  # noqa: E501
 
 if __name__ == "__main__":
   Gui(page).run(title="Status - With mixed icons")
